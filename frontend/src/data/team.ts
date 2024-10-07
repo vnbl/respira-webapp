c96a15e7edc0fcf00efd58632defbc28d2406a55
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-10-07 18:52:35 -0300

    add team members

--- frontend/src/data/team.ts
+++ frontend/src/data/team.ts
@@ -1,28 +1,92 @@
-export const TEAM = [
+import type { Image } from "./images";
+
+import  Fernanda from "../assets/team/fernanda_carles.png"
+import  Alvaro from "../assets/team/alvaro_machuca.png"
+import Sol from "../assets/team/sol_benitez.png"
+import Liliana from "../assets/team/liliana_estigarribia.png"
+import Carlos from "../assets/team/carlos_sauer.png"
+import Carolina from "../assets/team/carolina_recalde.png"
+import Diego from "../assets/team/diego_stalder.png"
+import Fabian from "../assets/team/fabian_bozzolo.png"
+import Luis from "../assets/team/luis_bernal.png"
+import Clara from "../assets/team/clara_berendsen.png"
+
+type TEAM_CARD = {
+    name: string;
+    title: string;
+    image: Omit<Image, "alt">
+}
+
+
+export const TEAM : TEAM_CARD []= [
     {
         name: "Fernanda Carles",
-        titulo: "Directora",
-        path: ""
+        title: "Project Leader, Data Scientist and Developer",
+        image: {
+            path: Fernanda
+        }
     },
     {
-        name: "Fernanda Carles",
-        path: ""
+        name: "Álvaro Machuca",
+        title: "Software Architect, Developer",
+        image: {
+            path: Alvaro
+        }
     },
     {
-        name: "Fernanda Carles",
-        path: ""
+        name: "Soledad Benítez",
+        title: "Product Designer",
+        image: {
+            path: Sol
+        }
     },
     {
-        name: "Fernanda Carles",
-        titulo: "Directora",
-        path: ""
+        name: "Clara Berendsen",
+        title: "Frontend Developer",
+        image: {
+            path: Clara
+        }
     },
     {
-        name: "Fernanda Carles",
-        path: ""
+        name: "Liliana Estigarribia",
+        title: "Project Coordinator",
+        image: {
+            path: Liliana
+        }
     },
     {
-        name: "Fernanda Carles",
-        path: ""
-    }        
+        name: "Fabián Bozzolo",
+        title: "Communication",
+        image: {
+            path: Fabian
+        }
+    },      
+    {
+        name: "Diego Stalder",
+        title: "Data Science Advisor - FIUNA",
+        image: {
+            path: Diego
+        }
+    },  
+    {
+        name: "Carlos Sauer",
+        title: "Data Science Advisor - FIUNA",
+        image: {
+            path: Carlos
+        }
+    },  
+    {
+        name: "Luis Bernal",
+        title: "Database Manager - FIUNA",
+        image: {
+            path: Luis
+        }
+    }, 
+    {
+        name: "Carolina Recalde",
+        title: "Especialista en Monitoreo Ambiental - FIUNA",
+        image: {
+            path: Carolina
+        }
+    }, 
 ]
