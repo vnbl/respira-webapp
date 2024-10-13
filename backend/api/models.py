fa859231a141d9449cf15c5f8b00ced6a366530a
quanturtle <alvaromachucar@gmail.com>
2024-10-13 16:23:04 -0300

    working final model, changed gunicorn to 4 workers

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,16 +1,15 @@
 from typing import List
 from django.db import models
 
+
 class RegionCodes(models.TextChoices):
     ASUNCION = ('ASUNCION', 'ASUNCION')
-    GRAN_ASUNCION = ('GRAN_ASUNCION', 'GRAN_ASUNCION')
-    CHACO = ('CHACO', 'CHACO')
 
 
 class Regions(models.Model):
-    name = models.CharField(max_length=255)
-    region_code = models.CharField(max_length=255, choices=RegionCodes.choices)
-    bbox = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    region_code = models.CharField(max_length=255, choices=RegionCodes.choices, blank=True, null=True)
+    bbox = models.CharField(max_length=255, blank=True, null=True)
     has_weather_data = models.BooleanField(default=False)
     has_pattern_station = models.BooleanField(default=False)
 
@@ -21,8 +20,8 @@ class Regions(models.Model):
 class Stations(models.Model):
     name = models.CharField(max_length=255)
     region = models.ForeignKey('Regions', on_delete=models.DO_NOTHING)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     is_station_on = models.BooleanField(default=False)
     is_pattern_station = models.BooleanField(default=False)
 
@@ -36,23 +35,16 @@ class Stations(models.Model):
 
 class RegionReadings(models.Model):
     region = models.ForeignKey('Regions', on_delete=models.DO_NOTHING)
-    date_localtime = models.DateTimeField()
-    pm1 = models.FloatField(blank=True, null=True)
-    pm2_5 = models.FloatField(blank=True, null=True)
-    pm10 = models.FloatField(blank=True, null=True)
-    pm2_5_avg_6h = models.FloatField(blank=True, null=True)
-    pm2_5_max_6h = models.FloatField(blank=True, null=True)
-    pm2_5_skew_6h = models.FloatField(blank=True, null=True)
-    pm2_5_std_6h = models.FloatField(blank=True, null=True)
-    aqi_pm2_5 = models.FloatField(blank=True, null=True)
-    aqi_pm10 = models.FloatField(blank=True, null=True)
-    aqi_level = models.IntegerField(blank=True, null=True)
-    aqi_pm2_5_max_24h = models.FloatField(blank=True, null=True)
-    aqi_pm2_5_skew_24h = models.FloatField(blank=True, null=True)
-    aqi_pm2_5_std_24h = models.FloatField(blank=True, null=True)
-    temperature = models.FloatField(blank=True, null=True)
-    humidity = models.FloatField(blank=True, null=True)
-    pressure = models.FloatField(blank=True, null=True)
+    date_utc = models.DateTimeField()
+    pm2_5_region_avg = models.FloatField(blank=True, null=True)
+    pm2_5_region_max = models.FloatField(blank=True, null=True)
+    pm2_5_region_skew = models.FloatField(blank=True, null=True)
+    pm2_5_region_std = models.FloatField(blank=True, null=True)
+    aqi_region_avg = models.FloatField(blank=True, null=True)
+    aqi_region_max = models.FloatField(blank=True, null=True)
+    aqi_region_skew = models.FloatField(blank=True, null=True)
+    aqi_region_std = models.FloatField(blank=True, null=True)
+    level_region_max = models.FloatField(blank=True, null=True)
 
     class Meta:
         db_table = 'region_readings'
@@ -60,7 +52,9 @@ class RegionReadings(models.Model):
 
 class StationReadingsGold(models.Model):
     station = models.ForeignKey('Stations', on_delete=models.DO_NOTHING)
-    date_localtime = models.DateTimeField()
+    airnow_id = models.IntegerField(blank=True, null=True)
+    date_utc = models.DateTimeField(blank=True, null=True)
+    pm_calibrated = models.BooleanField(blank=True, null=True)
     pm1 = models.FloatField(blank=True, null=True)
     pm2_5 = models.FloatField(blank=True, null=True)
     pm10 = models.FloatField(blank=True, null=True)
@@ -74,16 +68,13 @@ class StationReadingsGold(models.Model):
     aqi_pm2_5_max_24h = models.FloatField(blank=True, null=True)
     aqi_pm2_5_skew_24h = models.FloatField(blank=True, null=True)
     aqi_pm2_5_std_24h = models.FloatField(blank=True, null=True)
-    temperature = models.FloatField(blank=True, null=True)
-    humidity = models.FloatField(blank=True, null=True)
-    pressure = models.FloatField(blank=True, null=True)
 
     class Meta:
         db_table = 'station_readings_gold'
 
 
 class InferenceRuns(models.Model):
-    run_date = models.DateTimeField(auto_now=True)
+    run_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         db_table = 'inference_runs'
@@ -92,7 +83,6 @@ class InferenceRuns(models.Model):
 class InferenceResults(models.Model):
     inference_run = models.ForeignKey('InferenceRuns', on_delete=models.DO_NOTHING)
     station = models.ForeignKey('Stations', on_delete=models.DO_NOTHING)
-
     forecast_6h = models.JSONField(blank=True, null=True)
     forecast_12h = models.JSONField(blank=True, null=True)
     aqi_input = models.JSONField(blank=True, null=True)
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ chmod -R o+r /static;
 chmod -R o+x /static;
 chown -R www-data:www-data /static;
 
-gunicorn --bind :8000 --workers 2 backend.wsgi:application;
+gunicorn --bind :8000 --workers 4 backend.wsgi:application;
 
 echo "Django failed, but keeping the container alive..."
 tail -f /dev/null
