ee5256e0f9148f87bf2e5af049fead7672d36881
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-10-17 18:14:36 -0300

    cors configuration for the backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",  
     'rest_framework',
     'drf_spectacular',
     'api'
@@ -63,6 +64,7 @@ SPECTACULAR_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,3 +162,9 @@ STATIC_ROOT = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS configuration 
+#  See https://github.com/adamchainz/django-cors-headers for details
+
+CORS_ALLOWED_ORIGINS =  os.getenv('CORS_ALLOWED_ORIGINS').split(",")
