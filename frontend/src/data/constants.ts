c99fa3914bd513d35c949c8f55cf5ba2deb041ed
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-10-27 17:46:40 -0300

    add dev frontend

---
+++ frontend/src/data/constants.ts
@@ -0,0 +1,6 @@
+export const TWITTER_URL="https://twitter.com/respirapy"
+export const TELEGRAM_URL ="https://t.me/proyectorespira" 
+export const FACEBOOK_URL="https://www.facebook.com/proyectorespirapy"
+export const INSTAGRAM_URL= "https://www.instagram.com/proyectorespirapy"
+export const CONTACT_MAIL="airefiuna@ing.una.py"
+export const GITHUB_URL="https://github.com/vnbl/Mozilla-Aire"
