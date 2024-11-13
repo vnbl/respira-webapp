8f566528920e3e6f1c52c70aad8bc44be1b467ab
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-11-13 05:11:25 -0300

    round chart tooltip numbers

--- frontend/src/components/react/BarChartNivo.tsx
+++ frontend/src/components/react/BarChartNivo.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-
-import { AQI_COLORS } from "../../data/constants";
-import { getAQIIndex } from "../../utils";
+import { getColorRange } from "../../utils";
 import { ResponsiveBar} from "@nivo/bar";
 import { timeFormat } from "d3-time-format";
 import { scaleTime } from "d3-scale";
 import { BarItem } from "./BarItem";
 
 
-const getColorRange = (aqi:number) => AQI_COLORS[getAQIIndex(aqi)];
-
-const customTooltip = ({ value } : {value:number}) => (
-  <div className="bg-black p-2 rounded flex flex-col text-white font-serif">
-    <p className="text-xs">{value}</p>
+const customTooltip = ({ value }: { value: number }) => (
+  <div className="flex flex-col rounded bg-black p-2 font-serif text-white">
+    <p className="text-xs">{Math.round(value)}</p>
   </div>
 );
 
@@ -26,28 +22,13 @@ export const BarChart = ({ data }: any) => {
 
   const formatter = timeFormat("%I %p");
   const timeScaleTicks: string[] = React.useMemo(() => {
-    const scale = scaleTime().domain([new Date(data[0].timestamp), new Date(data[data.length-1].timestamp)]);
-    const ticks = scale.ticks(data.length > 6 ? 6 : 10);
-    return ticks.map((tick) => formatter(tick));
-  }, data);
-  // const TopLine = ({series, slices}: BarCustomLayerProps<Number>) => {
-  //   return (
-  //     <g>
-  //       {rects.map((rect) => (
-  //         <rect
-  //           key={rect.id}
-  //           x={rect.x}
-  //           y={0}
-  //           width={rect.width}
-  //           height={rect.height}
-  //           style={{
-  //             fill: '#fafafa',
-  //           }}
-  //         />
-  //       ))}
-  //     </g>
-  //   )
-  // }
+    const scale = scaleTime().domain([
+      new Date(data[0].timestamp),
+      new Date(data[data.length - 1].timestamp),
+    ])
+    const ticks = scale.ticks(data.length > 6 ? 6 : 10)
+    return ticks.map((tick) => formatter(tick))
+  }, data)
 
   return (
     <ResponsiveBar
