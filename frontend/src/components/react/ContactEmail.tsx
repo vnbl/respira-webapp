f9885a1aba93387db0f33c4d843abd89ae76a086
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-11-21 12:05:50 -0300

    send email trough form

---
+++ frontend/src/components/react/ContactEmail.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import type { EmailInput } from '../../actions/index.tsx';
+import { BASE_URL } from '../../data/constants.ts';
+
+export function Email(props: EmailInput) {
+  const { name, lastname, motive, message, email } = props;
+
+  return (
+    <Html>
+      <Head />
+      <Preview>Dropbox reset your password</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Img
+            src={`${BASE_URL}/favicon.png`}
+            width="40"
+            height="33"
+            alt="Respira"
+          />
+          <Section>
+            <Text style={textBold}>Nombre</Text>
+            <Text style={text}>{name} {lastname}</Text>
+            <Text style={textBold}>Contacto</Text>
+            <Text style={text}>{email}</Text>
+            <Text style={textBold}>Motivo de consulta</Text>
+            <Text style={text}>{motive}</Text>
+            <Text style={textBold}>Mensaje</Text>
+            <Text style={text}>{message}</Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+}
+
+
+const main = {
+  backgroundColor: "#f6f9fc",
+  padding: "10px 0",
+};
+
+const container = {
+  backgroundColor: "#ffffff",
+  border: "1px solid #f0f0f0",
+  padding: "45px",
+};
+
+const text = {
+  fontSize: "16px",
+  fontFamily:
+    "'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
+  fontWeight: "300",
+  color: "#404040",
+  lineHeight: "26px",
+};
+const textBold = {
+  fontSize: "16px",
+  fontFamily:
+    "'Open Sans', 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
+  fontWeight: "600",
+  color: "#404040",
+  lineHeight: "26px",
+};
+const button = {
+  backgroundColor: "#007ee6",
+  borderRadius: "4px",
+  color: "#fff",
+  fontFamily: "'Open Sans', 'Helvetica Neue', Arial",
+  fontSize: "15px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  width: "210px",
+  padding: "14px 7px",
+};
+
+const anchor = {
+  textDecoration: "underline",
+};
+export default Email;
