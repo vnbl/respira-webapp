f4f7eccc8fe68c8d6e5cbfa4869a17ee8d96d446
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-11-24 09:26:35 -0300

    generalize server name

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '$BACKEND_HOST $BACKEND_PORT $CERT_NAME $PIPELINE_HOST $PIPELINE_PORT $FRONTEND_PORT $FRONTEND_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$BACKEND_HOST $BACKEND_PORT $CERT_NAME $PIPELINE_HOST $PIPELINE_PORT $FRONTEND_PORT $FRONTEND_HOST $SERVER_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
