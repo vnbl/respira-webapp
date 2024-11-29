4e5984ba1ffd3c7a9c693eac272614b2da17599c
Fernanda Carles <fernanda.carles@gmail.com>
2024-11-29 14:47:41 -0300

    Update constants.ts: github URL

--- frontend/src/data/constants.ts
+++ frontend/src/data/constants.ts
@@ -3,7 +3,7 @@ export const TELEGRAM_URL ="https://t.me/proyectorespira"
 export const FACEBOOK_URL="https://www.facebook.com/proyectorespirapy"
 export const INSTAGRAM_URL= "https://www.instagram.com/proyectorespirapy"
 export const CONTACT_MAIL="airefiuna@ing.una.py"
-export const GITHUB_URL="https://github.com/vnbl/Mozilla-Aire"
+export const GITHUB_URL="https://github.com/ProyectoRespira"
 
 
 
