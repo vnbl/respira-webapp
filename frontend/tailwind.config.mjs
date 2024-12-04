aa8d13cc4f31c763bc9d940e05f4d112de7153a3
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2024-12-04 12:20:58 -0300

    fix require import on tailwind.config

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
- export default withMT({
+import withMT from '@material-tailwind/react/utils/withMT';
+
+export default withMT({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
